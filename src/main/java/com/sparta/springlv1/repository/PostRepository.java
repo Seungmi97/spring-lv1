b133a0eb89c9a1fa78f4189252f1e5355ccde199
Seungmi97 <mi.seung97@gmail.com>
2024-02-25 05:29:03 +0900

    기능 구현

---
+++ src/main/java/com/sparta/springlv1/repository/PostRepository.java
@@ -0,0 +1,10 @@
+package com.sparta.springlv1.repository;
+
+import com.sparta.springlv1.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+    List<Post> findAllByOrderByCreatedAtDesc();
+}
